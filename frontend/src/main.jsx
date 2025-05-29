1cf565320a7f755ce7eaaa55f6dd6276a2de3714
Weiyu <1659969249@qq.com>
2025-05-29 13:16:34 -0700

    add css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import axios from 'axios';
+import './index.css';
 
 axios.defaults.withCredentials = true;
 
